2e020084797fcc88e0bbd371d4f4b45d2059dc02
Tiago Pádua <tiagolpadua@gmail.com>
2019-05-19 21:36:19 -0300

    c4

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -13,11 +13,18 @@ export default class Feed extends Component {
 
     return (
       <FlatList
+        style={{ marginTop: 20 }}
         keyExtractor={item => item.id}
         data={fotos}
         renderItem={({ item }) => (
           <View>
-            <Text>{item.usuario}</Text>
+            <View>
+              <Image
+                source={require('../../resources/img/alura.png')}
+                style={{ width: 40, height: 40 }}
+              />
+              <Text>{item.usuario}</Text>
+            </View>
             <Image
               source={require('../../resources/img/alura.png')}
               style={{ width, height: width }}
