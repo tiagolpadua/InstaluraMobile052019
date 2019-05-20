33455bff02ec4072092456605f72718127ccbf42
Tiago Pádua <tiagolpadua@gmail.com>
2019-05-19 21:31:40 -0300

    c3

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Image, Text, View, Dimensions } from 'react-native';
+import { FlatList, Image, Text, View, Dimensions } from 'react-native';
 
 const { width } = Dimensions.get('screen');
 
 export default class Feed extends Component {
   render() {
+    const fotos = [
+      { id: 1, usuario: 'rafael' },
+      { id: 2, usuario: 'alberto' },
+      { id: 3, usuario: 'vitor' },
+    ];
+
     return (
-      <View>
-        <Text>rafael</Text>
-        <Image source={require('../../resources/img/alura.png')} style={{ width, height: width }} />
-      </View>
+      <FlatList
+        keyExtractor={item => item.id}
+        data={fotos}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.usuario}</Text>
+            <Image
+              source={require('../../resources/img/alura.png')}
+              style={{ width, height: width }}
+            />
+          </View>
+        )}
+      />
     );
   }
 }
