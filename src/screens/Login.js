c4689cb18ef00a7e5a73f28b795b59dc6d280ce5
Tiago Pádua <tiagolpadua@gmail.com>
2019-06-02 10:33:01 -0300

    c9.2

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,35 @@ import { Button, Dimensions, StyleSheet, Text, TextInput, View } from 'react-nat
 
 const { width } = Dimensions.get('screen');
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuario: '',
+      senha: '',
+    };
+  }
+
+  efetuaLogin = () => {
+    const { usuario, senha } = this.state;
+    const uri = 'https://instalura-api.herokuapp.com/api/public/login';
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({
+        login: usuario,
+        senha,
+      }),
+      headers: new Headers({
+        'Content-type': 'application/json',
+      }),
+    };
+    fetch(uri, requestInfo)
+      .then(response => {
+        if (response.ok) return response.text();
+        throw new Error('Não foi possível efetuar login');
+      })
+      .then(token => console.warn(token));
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -10,15 +39,18 @@ export default class Login extends Component {
         <View style={styles.form}>
           <TextInput
             style={styles.input}
+            autoCapitalize="none"
             placeholder="Usuário..."
             onChangeText={texto => this.setState({ usuario: texto })}
           />
           <TextInput
             style={styles.input}
+            autoCapitalize="none"
+            secureTextEntry
             placeholder="Senha..."
             onChangeText={texto => this.setState({ senha: texto })}
           />
-          <Button title="Login" onPress={() => console.warn('Login')} />
+          <Button title="Login" onPress={this.efetuaLogin} />
         </View>
       </View>
     );
